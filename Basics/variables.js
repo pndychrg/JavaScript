d3799ebb0e7cb1dc6a11a9f2f7989b06f01b45f7
Chirag Pandey <pndy.chrg2003@gmail.com>
2022-11-24 22:20:03 +0530

    Initial commit

---
+++ Basics/variables.js
@@ -0,0 +1,11 @@
+//we can define variables through let and var 
+/* 
+var - old one {has no block snope, can be redeclared (let can't)}
+RUles - must only contain letters, digit or symbols ( $,_)
+        First character must not be a digit
+ */
+let message = "hello";
+
+name = "Chirag";
+console.log(name);
+console.log(message);
